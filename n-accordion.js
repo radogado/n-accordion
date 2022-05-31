60c0d3a9c6756ad2fdd479932499be703b958e7b
Radoslav Sharapanov <rado@me.com>
2022-05-31 14:30:33 +0300

    ,

--- n-accordion.js
+++ n-accordion.js
@@ -1,28 +1,30 @@
 // Component Accordion
 (function() {
 	const animate_options = el => { return { easing: "ease-in-out", duration: window.matchMedia("(prefers-reduced-motion: no-preference)").matches ? (el.dataset.duration * 1000 || getComputedStyle(el).getPropertyValue('--duration') * 1000 || 200) : 0 } };
-	const openAccordion = (el, callback) => {
-		el = el.querySelector(":scope > .n-accordion__content");
+	const accordionContent = el => el.querySelector(":scope > .n-accordion__content");
+	const openAccordion = (el) => {
+		el = accordionContent(el);
 		window.requestAnimationFrame(() => {
 			el.style.height = 0;
 			el.style.overflow = "hidden";
-			el.parentNode.setAttribute("data-expanded", true);
-			el.parentNode.querySelector(":scope > .n-accordion__label button").setAttribute("aria-expanded", true);
-			el.animate([{ height: 0 }, { height: `${el.scrollHeight}px` }], animate_options(el.parentNode)).onfinish = () => {
+			let wrapper = el.parentNode;
+			wrapper.setAttribute("data-expanded", true);
+			wrapper.querySelector(":scope > .n-accordion__label button").setAttribute("aria-expanded", true);
+			el.animate([{ height: 0 }, { height: `${el.scrollHeight}px` }], animate_options(wrapper)).onfinish = () => {
 				el.style.height = el.style.overflow = "";
-				typeof callback !== 'function' || callback();
 			};
 		});
 	};
 	const closeAccordion = (el, callback) => {
-		el = el.querySelector(":scope > .n-accordion__content");
+		el = accordionContent(el);
 		window.requestAnimationFrame(() => {
 			// el.parentNode.open = true;
 			el.style.overflow = "hidden";
-			el.animate([{ height: `${el.scrollHeight}px` }, { height: 0 }], animate_options(el.parentNode)).onfinish = () => {
+			let wrapper = el.parentNode;
+			el.animate([{ height: `${el.scrollHeight}px` }, { height: 0 }], animate_options(wrapper)).onfinish = () => {
 				el.style.height = el.style.overflow = "";
-				el.parentNode.removeAttribute("data-expanded");
-				el.parentNode.querySelector(":scope > .n-accordion__label button").removeAttribute("aria-expanded");
+				wrapper.removeAttribute("data-expanded");
+				wrapper.querySelector(":scope > .n-accordion__label button").removeAttribute("aria-expanded");
 				typeof callback !== 'function' || callback();
 			};
 		});
@@ -30,19 +32,22 @@
 	const toggleAccordion = (e) => {
 		let el = e.target.closest('.n-accordion'); // el = .n-accordion
 		if (!el.getAttribute('data-expanded')) {
-			let container = el.closest(".n-accordion__popin");
-			if (container) {
-				let row = Math.floor(([...container.children].indexOf(el) / getComputedStyle(container).getPropertyValue("--n-popin-columns")) * 1) + 2;
-				container.style.setProperty("--n-popin-open-row", row);
-			}
-			if (el.parentNode.matches('[role="group"]') || container) {
+			let popin = el.closest(".n-accordion__popin");
+			const updateRow = () => {
+				if (popin) {
+					let row = Math.floor(([...popin.children].indexOf(el) / getComputedStyle(popin).getPropertyValue("--n-popin-columns")) * 1) + 2;
+					popin.style.setProperty("--n-popin-open-row", row);
+				}
+			};
+			if (el.parentNode.matches('[role="group"]') || popin) {
 				let other_accordion = el.parentNode.querySelector(":scope > [data-expanded]");
 				if (other_accordion) {
 					closeAccordion(other_accordion, () => { // el = .n-accordion
-						// console.log(el);
+						updateRow();
 						openAccordion(el);
 					});
 				} else {
+					updateRow();
 					openAccordion(el);
 				}
 			} else {
